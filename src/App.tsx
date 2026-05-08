3f7aff083d46274fb6e9b8707c043996a894f571
marui017 <7marui.works@gmail.com>
2026-05-08 23:39:56 +0900

    HashRouterに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,56 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import HomePage from './pages/HomePage'
-import WorksPage from './pages/WorksPage'
-import WorkDetailPage from './pages/WorkDetailPage'
-import AboutPage from './pages/AboutPage'
-import ContactPage from './pages/ContactPage'
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import WorksPage from "./pages/WorksPage";
+import WorkDetailPage from "./pages/WorkDetailPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<Layout><HomePage /></Layout>} />
-        <Route path="/works" element={<Layout><WorksPage /></Layout>} />
-        <Route path="/works/:id" element={<Layout><WorkDetailPage /></Layout>} />
-        <Route path="/about" element={<Layout><AboutPage /></Layout>} />
-        <Route path="/contact" element={<Layout><ContactPage /></Layout>} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <HomePage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/works"
+          element={
+            <Layout>
+              <WorksPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/works/:id"
+          element={
+            <Layout>
+              <WorkDetailPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <Layout>
+              <AboutPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <Layout>
+              <ContactPage />
+            </Layout>
+          }
+        />
       </Routes>
-    </BrowserRouter>
-  )
+    </HashRouter>
+  );
 }
